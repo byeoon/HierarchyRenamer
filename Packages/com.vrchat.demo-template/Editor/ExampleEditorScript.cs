12a71ed27dd593201f1d281feda53010909e416d
byron <47872200+byeoon@users.noreply.github.com>
2024-03-18 22:00:00 -0400

    Added other menu buttons

--- Packages/com.vrchat.demo-template/Editor/ExampleEditorScript.cs
+++ Packages/com.vrchat.demo-template/Editor/ExampleEditorScript.cs
@@ -1,10 +1,23 @@
 ﻿using UnityEditor;
+using System.Diagnostics;
 
 public class ExampleEditorScript
 {
-    [MenuItem("byeoon/HierarchyRenamer ")]
-    static void Test()
+    [MenuItem("byeoon/HierarchyRenamer")]
+    static void RunMainScript()
     {
-        EditorUtility.DisplayDialog("HierarchyRenamer", "Hello world!", "OK");
+        EditorUtility.DisplayDialog("HierarchyRenamer", "Hello world!", "Hello there!!");
+    }
+
+    [MenuItem("byeoon/Other/About")]
+    static void GetAbout()
+    {
+        EditorUtility.DisplayDialog("HierarchyRenamer", "HierarchyRenamer is currently running version 1.0", "OK");
+    }
+
+      [MenuItem("byeoon/Other/Open GitHub Repository")]
+    static void openRepository  ()
+    {
+        System.Diagnostics.Process.Start("explorer.exe", "http://github.com/byeoon/HierarchyRenamer");
     }
 }
